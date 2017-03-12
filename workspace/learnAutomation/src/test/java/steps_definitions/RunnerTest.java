cf7e892f517635459af7dc591f48b3808deafa63
Roland <roland.viss@mail.ru>
2017-03-13 00:56:17 +0200

    test automation

---
+++ workspace/learnAutomation/src/test/java/steps_definitions/RunnerTest.java
@@ -0,0 +1,16 @@
+package steps_definitions;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+	    //format = {"pretty", "json:target/"},
+	    features = {"src/test/resources/"}
+)
+
+public class RunnerTest {
+
+}
